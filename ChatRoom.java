f83c0999b23dae82410bd81896c7f5e984e75c0f
jeb1117 <30914289+jeb1117@users.noreply.github.com>
2018-04-18 15:36:36 -0600

    Eh

--- ChatRoom.java
+++ ChatRoom.java
@@ -1,16 +1,38 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ChatRoom
 {
   static Scanner write = new Scanner(System.in);
   public static final int DEFAULT_PORT = 8029;
-  public static void main(String[] args) throws IOException, UnknownHostException{
+  private static ArrayList<Socket> clientChat;
+  ServerSocket sock = new ServerSocket(DEFAULT_PORT);
+  DataInputStream chat;
+  DataOutputStream ret;
+  public ChatRoom() throws IOException
+  {
+	sock = new ServerSocket(3);
+	clientChat = new ArrayList<Socket>();
+	System.out.print("Waiting for connections...");
+	
+	
+  }
+  
+  public void addClient(Socket client) throws IOException
+  {
+	  client = sock.accept();
+	  clientChat.add(client);
+	  System.out.println("A new person has joined");
+	  
+  }
+  
+  public void checkIn()
+  {
+	  
+  }
 
-      Socket sock = new Socket(DEFAULT_PORT);
-      DataInputStream chat = new DataInputStream(sock.getInputStream());
-      DataOutputStream ret = new DataOutputStream(sock.getOutputStream());
 
       Thread sendIt = new Thread(new Runnable()
       {
@@ -51,5 +73,7 @@ public class ChatRoom
       readIt.start();
 
 
-  }
+      public static void main(String[] args) throws IOException{
+    	  ChatRoom server = new ChatRoom();
+      }
 }
--- Handler.java
+++ Handler.java
@@ -1,27 +1,13 @@
 import java.io.FileNotFoundException;
+import java.net.Socket;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
 public class Handler
 {
-  public static String chatroom_json(String obj)
-  {
-    Schema.SObjectType target = Schema.getGlobalDescribe().get(obj);
-    Schema.DescribeSObjectResult fieldType = target.getDescribe();
-    Map<String, Schema.SObjectType> fields = fieldType.fields.getMap();
-
-    List<Object> cats = new List<Object>();
-    for (String key : fields.keySet())
-    {
-      Schema.DescribeFieldResult dealio = fields.get(key).getDescribe();
-      Map<String, Object> size = new Map<String, Object>();
-
-      size.put('type', dealio.getType().name());
-      size.put('username', dealio.getName());
-      size.put('len', )
-    }
-  }
+	public void process(Socket client) throws java.io.IOException 
+	{
+		
+	}
 
 }
