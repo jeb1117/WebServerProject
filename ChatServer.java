7e8eba70b7059b53e7436972583ad8c5dfbb8786
KelseyMorgue <kelseymorgue666@gmail.com>
2018-04-25 14:57:39 -0600

    ChatServer skeleton + comments & Readme

---
+++ ChatServer.java
@@ -0,0 +1,62 @@
+/**
+* ChatServer.java
+* Server that runs on port 8029, following
+* "What's the Dealio" Protocol from CMPT 352
+* @author Kelsey Henrichsen and Jasmine Boonyakiti
+**/
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import org.json.simple.JSONObject;
+import java.util.Vector; //most likely based on Cole's explanation
+import java.util.HashMap; //most likely based on Cole's explanation
+
+public class ChatServer
+{
+    public static final int PORT = 8029;
+    public static final int maxClients; //declare how many l8r
+
+    // create thread pool --> https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/Executors.html
+    private static final Executor executor = Executors.newCachedThreadPool();
+
+
+
+    public static void main(String[] args) throws java.io.IOException {
+
+        // create a server socket listening to port 8029
+        ServerSocket server = new ServerSocket(PORT);
+        System.out.println("Waiting for connections ....");
+        
+        // run & execute chatroom
+
+        try
+        {
+            // makes sure that we are still within the chatroom client limits
+            for(int i=0; i < maxClients; i++)
+            {
+                //add to clients until max reached
+            }
+
+            while (true) 
+            { 
+                // infinite loop in order to listen for new clients
+                Socket client = server.accept();
+
+                // add new client socket connection
+                // using runnable and execute
+                
+            }
+        }
+
+        catch (IOException ioe)
+        {
+            System.out.println("IO Exception Thrown");
+        }
+
+        finally
+        {
+            // close the socket
+            server.close();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Chatroom Project
+## Synopsis
+
+This is a chatroom project that follows the "What's The Dealio" protocol for CMPT 352. 
+
+Write a client-server chat room application. The program will operate as follows: The server application listens to a specified port for client connections who wish to join the chat room. The server keeps track of all client connections in the chat room. A client joins the chatroom according to their username - their name for the chat session. Once the client has connected to the chatroom server, the client may then send a message to the chatroom where it will be broadcast to all members of the chatroom. A client may also send a private message to one other member in the chatroom.
+
+## Contributors
+
+Jasmine Boonyakiti
+Kelsey Henrichsen
+
+## Running Instructions
+
+How to Run:
+
+## Extras
+
